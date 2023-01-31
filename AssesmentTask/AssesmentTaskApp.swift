e653012bf5fb0acd63852bc9f997ae6979920913
UsamaNaseer <>
2023-01-31 15:27:25 +0500

    init proect

---
+++ AssesmentTask/AssesmentTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssesmentTaskApp.swift
+//  AssesmentTask
+//
+//  Created by Usama Naseer on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssesmentTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
